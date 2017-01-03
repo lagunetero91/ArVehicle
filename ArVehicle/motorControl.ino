72378d3748292cd1ae45fee6c3853e01b34b1c34
David Leon Ortega <david_lagu@hotmail.com>
2017-01-03 19:56:51 +0000

    Selección de comando

--- ArVehicle/motorControl.ino
+++ ArVehicle/motorControl.ino
@@ -1,5 +1,22 @@
 
 uint8_t moveVehicle(uint8_t dir){
+  switch(dir){
+    case 1:
+      goRight();
+      break;
+    case 2:
+      goLeft();
+      break;
+    case 3:
+      goUp();
+      break;
+    case 4:
+      goDown(); 
+      break;
+    default:
+      return 1;
+      break;
+  }
   return 0;
 }
 
