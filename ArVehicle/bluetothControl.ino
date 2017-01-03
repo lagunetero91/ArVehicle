613759cd0b917f1e368d9b84fc92cc92a8c7ef07
David Leon Ortega <david_lagu@hotmail.com>
2017-01-03 19:04:07 +0000

    Función que recibe el comando por BlueTooth implementada.

--- ArVehicle/bluetothControl.ino
+++ ArVehicle/bluetothControl.ino
@@ -1,5 +1,14 @@
 
 uint8_t receiveCommand(){
+  if(BTSerial.available()>0){
+    command = BTSerial.readString();
+    return 0;
+  }
+  return 1; // No comando
+}
+
+uint8_t do_OK(){
+  BTSerial.println("OK");
   return 0;
 }
 
