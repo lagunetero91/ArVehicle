6226eb6afb47a7f362d5fcd0c83bb7a80166fd52
David Leon Ortega <david_lagu@hotmail.com>
2017-01-03 19:03:08 +0000

    Pines usados definidos.

--- ArVehicle/Arvehicle.h
+++ ArVehicle/Arvehicle.h
@@ -1,5 +1,22 @@
+#define _DEBUG  //Descomentar para ver mensaje de depuración
 
+// Pines
+#define pinRX 7
+#define pinTX 8
+
+#define rightMotorA A0
+#define rightMotorB A1
+#define leftMotorA  A2
+#define leftMotorB  A3
+
+// Commandos
 #define movRigh 1
 #define movLeft 2
 #define goAhead 3
 #define goBack  4
+
+String command;
+
+//software serial
+SoftwareSerial BTSerial(7,8);
+
