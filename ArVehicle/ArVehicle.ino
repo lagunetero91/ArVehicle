89fda1cf66eca76b50907629076180b4bb0593bf
David Leon Ortega <david_lagu@hotmail.com>
2017-01-03 19:03:37 +0000

    Comienzo del program principal

--- ArVehicle/ArVehicle.ino
+++ ArVehicle/ArVehicle.ino
@@ -1,10 +1,21 @@
+#include <SoftwareSerial.h>
+#include "Arvehicle.h"
 
 void setup() {
-  // put your setup code here, to run once:
-
+  #if defined(_DEBUG)
+    Serial.begin(9600); //Iniciamos puerto serie para mensajes de deputación.
+  #endif
+    BTSerial.begin(9600); //Iniciamos comunicación serie con el módulo Bluetooth.
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(receiveCommand() == 0){
+   uint8_t status = moveVehicle((uint8_t)atoi(command.c_str()));
+   #if defined(_DEBUG)
+    Serial.print("moveVehicle devolvió: "); 
+    Serial.println(status);
+  #endif
+  }
+  
 }
